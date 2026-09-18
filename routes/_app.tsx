649e8dc583d2c761c26462b802a751dbcd907e91
Martin Vích <mail@martinvich.net>
2026-09-18 20:36:52 +0200

    Faktoromat logo and favicons

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,6 +7,21 @@ export default define.page(function App({ Component }) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="color-scheme" content="light" />
+        <meta name="theme-color" content="#183e2a" />
+        <meta name="apple-mobile-web-app-title" content="Fakturomat" />
+        <link rel="icon" href="/favicon.ico?v=2" sizes="16x16 32x32 48x48" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" sizes="any" />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-touch-icon.png"
+          sizes="180x180"
+        />
+        <link
+          rel="mask-icon"
+          href="/safari-pinned-tab.svg"
+          {...{ color: "#183e2a" }}
+        />
+        <link rel="manifest" href="/site.webmanifest" />
         <title>Fakturomat</title>
       </head>
       <body>
